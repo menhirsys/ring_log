82bb51ca5f414fbc667bca79a97f11959f93bbee
Edmund Huber <me@ehuber.info>
2017-06-02 00:13:14 -0700

    add FreeRTOS arch

---
+++ ring_log_arch_freertos.c
@@ -0,0 +1,28 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+
+#include "ring_log.h"
+
+static SemaphoreHandle_t mutex = NULL;
+static StaticSemaphore_t static_semaphore;
+
+void ring_log_arch_abort(void) {
+    vTaskDelete(NULL);
+}
+
+void ring_log_arch_init(void) {
+    RING_LOG_EXPECT(mutex, NULL);
+    mutex = xSemaphoreCreateMutexStatic(&static_semaphore);
+    RING_LOG_EXPECT_NOT(mutex, NULL);
+}
+
+void ring_log_arch_take_mutex(void) {
+    RING_LOG_EXPECT_NOT(mutex, NULL);
+    xSemaphoreTake(mutex, portMAX_DELAY);
+}
+
+void ring_log_arch_free_mutex(void) {
+    RING_LOG_EXPECT_NOT(mutex, NULL);
+    xSemaphoreGive(mutex);
+}
